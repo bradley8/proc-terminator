e7e24957655a8e6b42eacaf2543de619c1e43443
AutoM8 <bradley888@gmail.com>
2020-04-28 09:16:01 -0500

    Add files via upload

---
+++ ProcTerminator/ProcessTerminator.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace ProcTerminator
+{
+    class ProcessTerminator
+    {
+        internal bool TerminateProcesses(string[] processNames)
+        {
+            var procs = new List<Process>();
+            Console.Clear();
+            DisplayBanner();
+
+            foreach (var name in processNames)
+            {
+                procs.AddRange(Process.GetProcessesByName(name));
+            }
+
+            Console.WriteLine("------------------------------------------------------------");
+
+            if (procs.Count > 0)
+            {
+                foreach (var proc in procs)
+                {
+                    if (!proc.HasExited)
+                    {
+                        Console.WriteLine(proc + " process terminated");
+                        proc.Kill();
+                    }
+                }
+            }
+            else
+            {
+                foreach (var name in processNames)
+                {
+                    Console.WriteLine("No " + name + " processes to terminate");
+                }
+            }
+
+            Console.WriteLine("------------------------------------------------------------");
+
+            return true;
+        }
+
+        /// <summary>
+        /// Runs procterminator quietly, helpful when called from the command line and no UI is desired.
+        /// </summary>
+        /// <param name="processNames"></param>
+        /// <returns></returns>
+        internal bool TerminateProcessesQuietly(string[] processNames)
+        {
+            var procs = new List<Process>();
+            
+            foreach (var name in processNames)
+            {
+                procs.AddRange(Process.GetProcessesByName(name));
+            }
+
+            if (procs.Count > 0)
+            {
+                foreach (var proc in procs)
+                {
+                    if (!proc.HasExited)
+                    {
+                        proc.Kill();
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public void DisplayBanner()
+        {
+            // Display the banner.
+            Console.WriteLine(@"___  ____ ____ ____    ___ ____ ____ _  _ _ _  _ ____ ___ ____ ____ ");
+            Console.WriteLine(@"|__] |__/ |  | |        |  |___ |__/ |\/| | |\ | |__|  |  |  | |__/ ");
+            Console.WriteLine(@"|    |  \ |__| |___     |  |___ |  \ |  | | | \| |  |  |  |__| |  \ v3.1");
+            Console.WriteLine(@"------------------------------------------------------------------------");
+            Console.WriteLine(@"B-radical Software 2018");
+            Console.WriteLine(@"C3 Certified");
+            Console.WriteLine(@"");
+
+        }
+    }
+}
---
+++ ProcTerminator/TerminatorApp.cs
@@ -0,0 +1,96 @@
+﻿using System;
+
+namespace ProcTerminator
+{
+    class TerminatorApp
+    {
+        static void Main(string[] args)
+        {
+            // Terminator object. Used to terminate processes.
+            ProcessTerminator terminator = new ProcessTerminator();
+
+            // If there are command line arguments, run terminator quietly without a UI.
+            if (args.Length > 0)
+            {
+                terminator.TerminateProcessesQuietly(args);
+            }
+            else
+            {
+                // Holds the user's menu choice.
+                string choice;
+
+                // Display the banner.
+                terminator.DisplayBanner();
+
+                // Display menu options until user types "e" to exit.
+                do
+                {
+                    // Display menu options.
+                    Console.WriteLine("(Press)");
+                    Console.WriteLine("(1) To Terminate Chrome processes");
+                    Console.WriteLine("(2) To Terminate Firefox processes");
+                    Console.WriteLine("(3) To Terminate IE processes");
+                    Console.WriteLine("(4) To Terminate Edge processes");
+                    Console.WriteLine("(5) To Terminate browser processes");
+                    Console.WriteLine("(6) To Terminate ALM processes");
+                    Console.WriteLine("(7) To Terminate UFT processes");
+                    Console.WriteLine("Or e to exit");
+
+                    // Get the user's choice
+                    choice = Console.ReadLine().ToLower().Trim();
+
+                    // Act on the choice
+                    switch (choice)
+                    {
+                        case "1":
+                            // Terminate Chrome processes
+                            terminator.TerminateProcesses(new string[] { "chrome", "chromedriver" });
+                            break;
+
+                        case "2":
+                            // Terminate Firefox processes
+                            terminator.TerminateProcesses(new string[] { "firefox", "geckodriver" });
+                            break;
+
+                        case "3":
+                            // Terminate Internet Explorer processes
+                            terminator.TerminateProcesses(new string[] { "iexplore", "iedriverserver" });
+                            break;
+
+                        case "4":
+                            // Terminate Internet Explorer processes
+                            terminator.TerminateProcesses(new string[] { "microsoftedge", "microsoftwebdriver" });
+                            break;
+
+                        case "5":
+                            // Termine all processes
+                            terminator.TerminateProcesses(new string[]
+                            { 
+                              "chrome",
+                              "chromedriver",
+                              "firefox",
+                              "geckodriver",
+                              "iexplore", 
+                              "iedriverserver",
+                              "microsoftedge",
+                              "microsoftwebdriver"
+                            });
+                            break;
+
+                        case "6":
+                            // Terminate ALM Client processes
+                            terminator.TerminateProcesses(new string[] { "ALM-Client" });
+                            break;
+                                                        
+                        case "7":
+                            // Terminate UFT processes
+                            terminator.TerminateProcesses(new string[] { "UFT" });
+                            break;
+                            
+                    }
+
+                } while (choice != "e");
+            }
+        }
+    }
+}
